5c3bbdf302b42bdb6aeba48b71061e1681f9153c
Daniel <danielzietala@gmail.com>
2022-02-15 22:23:05 +0100

    Cleanup

--- job-offers/src/main/java/com/dany/joboffers/WebSecurityConfig.java
+++ job-offers/src/main/java/com/dany/joboffers/WebSecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -18,9 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                // by default uses a Bean by the name of corsConfigurationSource
-                .cors();
+        http.cors();
     }
 
     @Bean
